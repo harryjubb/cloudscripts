3422e798b55a1e7fb44eb8ebaabf7327aa527313
Harry Jubb <harryjubb@users.noreply.github.com>
2017-09-24 20:05:15 +0100

    Update start_murmur_server.sh

--- digitalocean/murmur/start_murmur_server.sh
+++ digitalocean/murmur/start_murmur_server.sh
@@ -5,5 +5,5 @@ curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DIGI
 export MAX_USERS=${MAX_USERS:=10}
 export IFTTT_EVENT=\\"${IFTTT_EVENT:=murmur_setup}\\"
 export IFTTT_KEY=\\"${IFTTT_KEY:=your_ifttt_recipe_key}\\"
-export IFTTT_EMAILS=\\"${IFTTT_EMAILS:=your_emails_to_alert}\" # SPACE OR COMMA SEPARATED
+export IFTTT_EMAILS=\\"${IFTTT_EMAILS:=your_emails_to_alert}\\" # SPACE OR COMMA SEPARATED
 bash <(curl https://raw.githubusercontent.com/harryjubb/cloudscripts/master/digitalocean/murmur/murmur_cloudinit.sh)"'","private_networking":null,"volumes": null,"tags":["mumble"]}' "https://api.digitalocean.com/v2/droplets"
