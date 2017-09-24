b46f2518207501ea11d159a643970e48f70ac9d4
Harry Jubb <harryjubb@users.noreply.github.com>
2017-09-24 20:04:41 +0100

    Update start_murmur_server.sh

--- digitalocean/murmur/start_murmur_server.sh
+++ digitalocean/murmur/start_murmur_server.sh
@@ -3,7 +3,7 @@ UUID="mumble-lon1-$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3,$4,
 
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" -d '{"name":"'"$UUID"'","region":"lon1","size":"512mb","image":"ubuntu-16-04-x64","ssh_keys":null,"backups":false,"ipv6":false,"user_data":"#!/bin/bash"'"
 export MAX_USERS=${MAX_USERS:=10}
-export IFTTT_EVENT=\"${IFTTT_EVENT:=murmur_setup}\"
-export IFTTT_KEY=\"${IFTTT_KEY:=your_ifttt_recipe_key}\"
-export IFTTT_EMAILS=\"${IFTTT_EMAILS:=your_emails_to_alert}\" # SPACE OR COMMA SEPARATED
+export IFTTT_EVENT=\\"${IFTTT_EVENT:=murmur_setup}\\"
+export IFTTT_KEY=\\"${IFTTT_KEY:=your_ifttt_recipe_key}\\"
+export IFTTT_EMAILS=\\"${IFTTT_EMAILS:=your_emails_to_alert}\" # SPACE OR COMMA SEPARATED
 bash <(curl https://raw.githubusercontent.com/harryjubb/cloudscripts/master/digitalocean/murmur/murmur_cloudinit.sh)"'","private_networking":null,"volumes": null,"tags":["mumble"]}' "https://api.digitalocean.com/v2/droplets"
