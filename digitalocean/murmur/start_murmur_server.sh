399cb97b7742bebbf75103176a342c5b5f798e64
Harry Jubb <harryjubb@users.noreply.github.com>
2017-09-24 20:05:59 +0100

    Update start_murmur_server.sh

--- digitalocean/murmur/start_murmur_server.sh
+++ digitalocean/murmur/start_murmur_server.sh
@@ -1,7 +1,7 @@
 DIGITALOCEAN_TOKEN="${DIGITALOCEAN_TOKEN:=your_token}"
 UUID="mumble-lon1-$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3,$4,$5,$6,$7$8$9}')" # https://serverfault.com/a/799198/157989
 
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" -d '{"name":"'"$UUID"'","region":"lon1","size":"512mb","image":"ubuntu-16-04-x64","ssh_keys":null,"backups":false,"ipv6":false,"user_data":"#!/bin/bash"'"
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" -d '{"name":"'"$UUID"'","region":"lon1","size":"512mb","image":"ubuntu-16-04-x64","ssh_keys":null,"backups":false,"ipv6":false,"user_data":"'"#!/bin/bash
 export MAX_USERS=${MAX_USERS:=10}
 export IFTTT_EVENT=\\"${IFTTT_EVENT:=murmur_setup}\\"
 export IFTTT_KEY=\\"${IFTTT_KEY:=your_ifttt_recipe_key}\\"
