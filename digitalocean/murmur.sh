591c512fe3ae5efb709a4936b1979b2cbb06b56e
Harry Jubb <harryjubb@users.noreply.github.com>
2017-09-24 03:30:18 +0100

    Update murmur.sh

--- digitalocean/murmur.sh
+++ digitalocean/murmur.sh
@@ -1,1 +1,41 @@
+#!/bin/sh
 
+# UPDATES
+apt-get update
+apt-get -y upgrade
+
+# PASSWORDS
+apt-get install -y pwgen
+
+SUPERUSER_PASSWORD="$(pwgen)"
+SERVER_PASSWORD="$(pwgen)"
+MAX_USERS=10
+
+# MUMBLE
+apt-get install -y mumble-server
+apt install debconf-utils
+
+# CONFIGURE SERVER
+echo "mumble-server   mumble-server/start_daemon      boolean true" | debconf-set-selections
+echo "mumble-server   mumble-server/use_capabilities  boolean true" | debconf-set-selections
+echo "mumble-server   mumble-server/password password $SUPERUSER_PASSWORD" | debconf-set-selections
+dpkg-reconfigure -f noninteractive mumble-server
+
+# EDIT INI FILE
+sed -i "s/^serverpassword=/serverpassword=$SERVER_PASSWORD/" /etc/mumble-server.ini
+sed -i "s/^users=100/users=$MAX_USERS/" /etc/mumble-server.ini
+sed -i "s/^#autoban/autoban/" /etc/mumble-server.ini # ENABLE AUTOBAN
+
+# RESTART
+service mumble-server restart
+
+# FIREWALL
+ufw allow OpenSSH
+ufw allow 64738 # MUMBLE/MURMUR
+ufw --force enable
+
+# ALERT SUCCESS
+PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+
+curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"your_email\",\"value2\":\"IP: $PUBLIC_IPV4 | SuperUser pass: $SUPERUSER_PASSWORD | Server pass: $SERVER_PASSWORD \"}" \
+https://maker.ifttt.com/trigger/your_trigger/with/key/your_key
